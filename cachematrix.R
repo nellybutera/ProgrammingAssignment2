b277cb8e8c6f8a2724475427d1b5706ed65ecd7a
Teta Butera Nelly <buteranelly250@gmail.com>
2026-09-12 22:43:46 +0200

    Add functions to save and reuse a matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Finding the inverse of a big matrix takes time. These two functions
+## work together so we only do that work once. The first time we ask
+## for the inverse, we calculate it and save it. Next time, we just
+## reuse the saved answer instead of calculating it again.
 
-## Write a short comment describing this function
+## This function does not calculate anything. It just builds a "box"
+## that can hold a matrix and, later, its inverse.
+## The box gives us four small tools:
+##   set()        - put a new matrix in the box
+##   get()        - take the matrix out of the box
+##   setinverse() - save the inverse in the box
+##   getinverse() - take the saved inverse out of the box
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function gives you the inverse of the matrix inside the box
+## made by makeCacheMatrix().
+## First it checks: did we already save the inverse before?
+##   - If yes, just hand back the saved one. No new work needed.
+##   - If no, calculate it now, save it in the box for next time,
+##     and then hand it back.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Check the box for an inverse we already saved
+        inv <- x$getinverse()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        ## No saved inverse yet, so calculate it now
+        data <- x$get()
+        inv <- solve(data, ...)
+        ## Save it in the box for next time
+        x$setinverse(inv)
+        inv
 }
